da957f7339d48a17d9b3c63a492b35ab88e5c656
Francisco Lima <fg.lima@campus.fct.unl.pt>
2025-04-09 16:47:07 +0100

    move dos clients e ContentsClient novo

--- src/fctreddit/impl/servers/java/JavaContent.java
+++ src/fctreddit/impl/servers/java/JavaContent.java
@@ -1,6 +1,25 @@
 package fctreddit.impl.servers.java;
 
+import java.util.List;
+import java.util.logging.Logger;
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.Response.Status;
+import fctreddit.api.User;
+import fctreddit.api.java.Result;
+import fctreddit.api.java.Users;
+import fctreddit.api.java.Result.ErrorCode;
+import fctreddit.impl.servers.persistence.Hibernate;
+import fctreddit.impl.clients.rest.RestImageClient;
+	
 public class JavaContent {
-    
+
+    private static Logger Log = Logger.getLogger(JavaContent.class.getName());
+
+	private Hibernate hibernate;
+	private RestImageClient imageClient;
+	
+	public JavaContent() {
+		hibernate = Hibernate.getInstance();
+	}
     
 }
